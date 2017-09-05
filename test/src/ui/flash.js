ec0de047880535ee6fdd0eb498406a370278403e
pavel.rimsky <pavel.rimsky@spika.com>
2017-09-05 09:44:40 +0200

    tests for the flash feature

---
+++ test/src/ui/flash.js
@@ -0,0 +1,101 @@
+var test = require("tape"),
+    fs = require('fs'),
+    testUtil = require("../common/testUtil");
+
+testUtil.setUp();
+
+var MARKER_ATTRIBUTE = "data-marker",
+    MARKER_ATTRIBUTE_SET_BY_PLUGIN = "setByPlugin",
+    MARKER_ATTRIBUTE_INITIAL_VALUE = "initialValue",
+    TEXT_VALUE_INITIAL = "textValueInitial",
+    TEXT_VALUE_MODIFIED_WITHOUT_FLASH = "textValueModifiedWithoutFlash",
+    TEXT_VALUE_MODIFIED_WITH_FLASH = "textValueModifiedWithFlash";
+
+function installPluginToElement(jqElement, t) {
+    jqElement.children().each(
+        function () {
+            t.equal(
+                $(this).attr(MARKER_ATTRIBUTE),
+                MARKER_ATTRIBUTE_INITIAL_VALUE,
+                "element correctly (re)initialized"
+            );
+            $(this).attr(MARKER_ATTRIBUTE, MARKER_ATTRIBUTE_SET_BY_PLUGIN);
+        }
+    );
+}
+
+test(
+    "flash",
+    function (t) {
+
+        var jqFixture =
+                testUtil.setUpFixture(
+                    fs.readFileSync(__dirname + "/flash.html")
+                );
+
+        jqFixture.children().first().yellowCode(
+            {
+                init: function(model) {
+                    model.textValue = TEXT_VALUE_INITIAL;
+                },
+
+                installPlugin: function(model, context) {
+                    installPluginToElement($(context.domElement), t);
+                },
+                
+                "@Public": {
+                    changeTextValueWithoutFlash: function(model) {
+                        model.textValue = TEXT_VALUE_MODIFIED_WITHOUT_FLASH;
+                    },
+
+                    changeTextValueWithFlash: function(model, context) {
+                        model.textValue = TEXT_VALUE_MODIFIED_WITH_FLASH;
+                        context.flash("myFlashableElement");
+                    }
+                }
+            }
+        );
+
+        t.equal(
+            jqFixture.find("span").attr(MARKER_ATTRIBUTE),
+            MARKER_ATTRIBUTE_INITIAL_VALUE,
+            "plugin not installed until DOM processed"
+        );
+        
+        setTimeout(
+            function() {
+                t.equal(
+                    jqFixture.find("span").attr(MARKER_ATTRIBUTE),
+                    MARKER_ATTRIBUTE_SET_BY_PLUGIN,
+                    "plugin installed when DOM processed"
+                );
+
+                jqFixture.children().first().yellowCode("getPublicApi").changeTextValueWithoutFlash();
+
+                t.equal(
+                    jqFixture.find("span").text(),
+                    TEXT_VALUE_INITIAL,
+                    "should not modify flashable element"
+                );
+
+                jqFixture.children().first().yellowCode("getPublicApi").changeTextValueWithFlash();
+
+                setTimeout(
+                    function() {
+                        t.equal(
+                            jqFixture.find("span").text(),
+                            TEXT_VALUE_MODIFIED_WITH_FLASH,
+                            "should modify flashable element with flash"
+                        );
+
+                        testUtil.removeFixture();
+                        t.end();
+                    },
+                    0
+                );
+            },
+            0
+        );
+
+    }
+);
